f870666e1e4eb4067717ff5f87984ade4b86b840
Jesusvu <36322537+Jesusvu@users.noreply.github.com>
2018-10-01 20:54:53 -0500

    Create VirtualPet.js

---
+++ VirtualPet.js
@@ -0,0 +1,72 @@
+$(function() {
+  
+  // Called function to update the name, happiness, and weight of our pet in our HTML
+  checkAndUpdatePetInfoInHtml();
+
+  // When each button is clicked, it will "call" function for that button (functions are below)
+  $('.treat-button').click(clickedTreatButton);
+  $('.play-button').click(clickedPlayButton);
+  $('.exercise-button').click(clickedExerciseButton);
+  
+})
+
+  // Add a variable "pet_info" equal to a dictionary with the name (string), weight (number), and happiness (number) of your pet
+
+  function clickedTreatButton() {
+    // Increase pet happiness
+    pet_info.happiness = pet_info.happiness + 1;
+    //pet_info.happiness++;
+    
+    // Increase pet weight
+    pet_info.weight = pet_info.weight + 1;
+    checkAndUpdatePetInfoInHtml();
+  }
+  
+  function clickedPlayButton() {
+    // Increase pet happiness
+    pet_info.happiness++;
+    
+    // Decrease pet weight
+    pet_info.weight--;
+    checkAndUpdatePetInfoInHtml();
+  }
+  
+  function clickedExerciseButton() {
+    // Decrease pet happiness
+    pet_info.happiness--;
+    
+    // Decrease pet weight
+    pet_info.weight--;
+    
+    checkAndUpdatePetInfoInHtml();
+  }
+
+  function checkAndUpdatePetInfoInHtml() {
+    checkWeightAndHappinessBeforeUpdating();  
+    updatePetInfoInHtml();
+  }
+  
+  function checkWeightAndHappinessBeforeUpdating() {
+    // Add conditional so if weight is lower than zero, set it back to zero
+    if (pet_info.weight < 0){
+      pet_info.weight = 0;
+   // }else if (pet_info.weight > 20){
+      //display message pet overweight
+    }
+   if (pet_info.happiness < 0){
+      pet_info.happiness = 0;   
+      }
+     
+  }
+  
+  // Updates your HTML with the current values in your pet_info dictionary
+  function updatePetInfoInHtml() {
+    $('.name').text(pet_info['name']);
+    
+    if (pet_info.weight > 20){
+      $('.weight').text(pet_info['weight'] + ' pounds, your pet is fat');
+     } else {
+    $('.weight').text(pet_info['weight']);
+    }
+    $('.happiness').text(pet_info['happiness']);
+  }
